1053d9ded70e00c26aeb39ab0aedfd3af90ad06c
Martynas Skaringa <skarware@gmail.com>
2020-07-02 23:02:27 +0300

    Initial commit

---
+++ src/main/java/org/martynas/currency_converter/Main.java
@@ -0,0 +1,14 @@
+package org.martynas.currency_converter;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(args)); //////////////////// FOR DEVELOPMENT PURPOSES ////////////////////
+
+    }
+
+
+}
